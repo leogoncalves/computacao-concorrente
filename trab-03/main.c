5960835fbc7a9c15ce11232b775df1dd501b8bf7
Leonardo Gonçalves <leonardo.goncalves.2711@gmail.com>
2021-02-27 18:57:39 -0300

    + Ajustes em métodos de reconhecimento de padrões, logs e buffer

--- trab-03/main.c
+++ trab-03/main.c
@@ -5,66 +5,92 @@
 
 #define DEBUG 1
 
-void find_identical_value_sequence(int* array, int array_size);
-int three_equals_values (int a, int b, int c);
-void find_continous_sequence_of_same_value(int* array, int array_size);
-void find_number_of_match_sequence(int *array, int array_size);
+typedef struct {
+    // position | max | sequence_value
+    long long int sequence_of_identical_values[3];
+    long long int amount_triples;
+    long long int occurrences_of_sequence;
+} Response;
 
-void read_from_binary_file(char* filename, int* buffer, long long int buffer_size, long long int offset);
+int three_equals_values (int a, int b, int c);
 long long int get_size(char* filename);
+
 void show_buffer(int* array, long long int array_size);
+void set_buffer(int* array, long long int array_size, int value);
+void showResponse(Response *response);
+void read_from_binary_file(char* filename, int* buffer, long long int buffer_size, long long int offset);
+void find_identical_value_sequence(int* array, int array_size, Response* response);
+void find_continous_sequence_of_same_value(int* array, int array_size, Response* response);
+void find_number_of_match_sequence(int *array, int array_size, Response* response);
+
 
 int main(int argc, char *argv[]) {
+
+    Response *response = (Response*) malloc(sizeof(Response));
+    for(int k = 0; k < 3; k++)
+        response->sequence_of_identical_values[k] = 0;
+    response->amount_triples = 0;
+    response->occurrences_of_sequence = 0;
 
     char* filenameControlTest = "input.bin";   
     // char* filenameRandomTest = "random_input.bin";
     
     /*
-        Definir estratégia para o tamanho padrão 
-        do buffer baseado na quantidade de elementos
-        informado no arquivo
+        Tamanho do arqui 
     */
-    // long long int size = get_size(filenameControlTest);
+    long long int size = get_size(filenameControlTest);
 
     /*
-        Tamanho do Bloco
+        Tamanho do Bloco (chunk do arquivo)
+        1 KB = 1024 bytes
+        1 MB = 1024 Kylobytes
+        1 GB = 1024 Megabytes
+
+        M = 1024 * 1024 * 1 = 1MB
+
     */
-    // long long int M;
+    long long int M = 1024 * 1024 * 1;
 
     /*
         Tamanho do buffer a ser utilizado pela aplicação
     */
-    // long long int N;
+    long long int N = 1024;
 
-
-    /*
-        Buffer que será utilizado pelas threads
-    */
+    int i = 1;
     
-    // long long int total_blocks = ceil(size / M);
-
-
-    int i = 0;
-    while(i < 3) {
+    long long int offset = 0;
+    long long int buffer_size = 20;
+    int* bufferT = (int*) malloc(buffer_size * sizeof(int));
+    set_buffer(bufferT, buffer_size, -1);
+    
+    while(i <= 2) {
         /*
             Aloca memória dinamicamente, a 
             depender do tamanho do bloco
             necessário
-        */   
-        long long int offset = 0;
-        long long int buffer_size = 10;
-        int* bufferT = (int*) malloc(buffer_size * sizeof(int));
-        
-        offset = buffer_size*i + offset;
-        printf("offset: %lld \n", offset);
-        
+        */
         read_from_binary_file(filenameControlTest, bufferT, buffer_size, offset);
-        find_identical_value_sequence(bufferT, buffer_size);
-        find_continous_sequence_of_same_value(bufferT, buffer_size);
-        find_number_of_match_sequence(bufferT, buffer_size);
+        find_identical_value_sequence(bufferT, buffer_size, response);
+        find_continous_sequence_of_same_value(bufferT, buffer_size, response);
+        find_number_of_match_sequence(bufferT, buffer_size, response);
         
+        offset += buffer_size;        
+        set_buffer(bufferT, buffer_size, -1);
         i++;
     }
+    showResponse(response);
+}
+
+void showResponse(Response *response) {
+    
+    printf("- Maior sequencia de valores identicos: \n");
+    printf("\t- Posição: %lld \n", response->sequence_of_identical_values[0]);
+    printf("\t- Tamanho: %lld \n", response->sequence_of_identical_values[1]);
+    printf("\t- Valor da sequencia que se repete: %lld \n", response->sequence_of_identical_values[2]);
+
+    printf("- Quantidade de triplas que se repetem: %lld \n", response->amount_triples);
+
+    printf("- Quantidade de ocorrencias da sequencia <012345>: %lld \n", response->occurrences_of_sequence);
 }
 
 /*
@@ -74,29 +100,40 @@ int main(int argc, char *argv[]) {
     @param array Ponteiro para array de inteiros
     @param array_size Quantidade de elementos no array
 */
-void find_identical_value_sequence(int* array, int array_size) {
+void find_identical_value_sequence(int* array, int array_size, Response* response) {
     int count = 1; 
     int max = 1;
     int sequence_value = -1;
     int position = -1;
 
     for(int i = 0; i < array_size; i++) {
+        if(array[i] == -1) {
+            break;
+        }
         if(array[i] == array[i+1]) {
             count++;
         } else {
             if(count > max) {
+                position = i - count + 1;
                 max = count;
                 sequence_value = array[i];
-                position = i - count;
             }
             count = 1;
         }
     }
 
-    printf("Maior sequencia de valores identicos: \n");
-    printf("\t- Posição: %d \n", position);
-    printf("\t- Tamanho: %d \n", max);
-    printf("\t- Valor da sequencia que se repete: %d \n", sequence_value);
+    if(max > response->sequence_of_identical_values[1]) {
+        response->sequence_of_identical_values[0] = position;
+        response->sequence_of_identical_values[1] = max;
+        response->sequence_of_identical_values[2] = sequence_value;
+    }
+
+    if(DEBUG) {
+        printf("[DEBUG] Maior sequencia de valores identicos: \n");
+        printf("\t- Posição: %d \n", position);
+        printf("\t- Tamanho: %d \n", max);
+        printf("\t- Valor da sequencia que se repete: %d \n", sequence_value);
+    }
 }
 
 /*
@@ -122,17 +159,24 @@ int three_equals_values (int a, int b, int c) {
     @param array Ponteiro para array de inteiros
     @param array_size Quantidade de elementos no array
 */
-void find_continous_sequence_of_same_value(int* array, int array_size) {
+void find_continous_sequence_of_same_value(int* array, int array_size, Response* response) {
     int count = 0;
     int i = 0;
     while(i < array_size) {
+        if(array[i] == -1) {
+            break;
+        }
         if(three_equals_values(array[i], array[i + 1], array[i + 2])) {            
             count++;
             i+=2;
         }
         i += 1;
     }
-    printf("Quantidade de triplas que se repetem: %d \n", count);
+    response->amount_triples += count;
+    if(DEBUG) {
+        printf("[DEBUG] Quantidade de triplas que se repetem: %d \n", count);
+    }
+    
 }
 
 /*
@@ -142,22 +186,35 @@ void find_continous_sequence_of_same_value(int* array, int array_size) {
     @param array Ponteiro para array de inteiros
     @param array_size Quantidade de elementos no array
 */
-void find_number_of_match_sequence(int *array, int array_size) {
+void find_number_of_match_sequence(int *array, int array_size, Response* response) {
+
+    if(DEBUG) {
+        printf("[DEBUG] Buffer em find_number_of_match_sequence \n");
+        show_buffer(array, array_size);
+    }
     
     // Parametros imutaveis
     int DEFAULT_SIZE_MATCH = 6;
     int PATTERN_MATCH[] = {0, 1, 2, 3, 4, 5};
+    long long int total_occurence = 0;
     
     for(int i = 0; i <= array_size - DEFAULT_SIZE_MATCH; i++ ) {
+        if(array[i] == -1) {
+            break;
+        }
         int j;
         for(j = 0; j < DEFAULT_SIZE_MATCH; j++) {
             if(array[i+j] != PATTERN_MATCH[j])
                 break;
         }
         if(j == DEFAULT_SIZE_MATCH) {
-            printf("Quantidade de ocorrencias da sequencia <012345>: %d \n", i);
+            total_occurence += 1;
+            if(DEBUG) {
+                printf("[DEBUG] Ocorrencias da sequencia <012345> na posição: %d \n", i);
+            }
         }
     }
+    response->occurrences_of_sequence = total_occurence;
 }
 
 /*
@@ -183,6 +240,11 @@ void read_from_binary_file(char* filename, int* buffer, long long int buffer_siz
         nada com ele
     */
     fseek(file, sizeof(long long int), 1);
+
+        
+    if(DEBUG) {
+        printf("[DEBUG] Offset em read_from_binary_file: %lld \n", offset);
+    }
 
     /*
         Offset para posição do arquivo que queremos ler
@@ -226,10 +288,17 @@ long long int get_size(char* filename) {
     @param array_size Tamanhho do vetor de inteiros
 */
 void show_buffer(int* array, long long int array_size) {
-    
+    if(DEBUG) {
+        printf("[DEBUG] show_buffer \n");
+    }
     for(int i = 0; i < array_size; i++) {
         printf("%d ", *(array + i));
     }
     printf("\n");
 }
-    
+
+void set_buffer(int* array, long long int array_size, int value) {
+    for(int i = 0; i < array_size; i++) {
+        array[i] = value;
+    }
+}
